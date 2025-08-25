8f6cbb846b98092e3c58ed69ab6aa23768a83598
JonJagger <jon@kosli.com>
2025-08-25 08:53:19 +0100

    Update install script, move to 1.14.0

--- docker/install.sh
+++ docker/install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -Eeu
 
+# From https://gist.github.com/Cartexius/4c437c084d6e388288201aadf9c8cdd5
+
 apt-get install --yes \
   cmake \
   libgtest-dev
 
-cd /usr/src/gtest
-cmake .
+mkdir -p $HOME/build
+cd $HOME/build
+cmake /usr/src/googletest/googletest
 make
-mv libg* /usr/lib
+cp lib/libgtest* /usr/lib/
+cd ..
+rm -rf build
+mkdir /usr/local/lib/googletest
+ln -s /usr/lib/libgtest.a /usr/local/lib/googletest/libgtest.a
+ln -s /usr/lib/libgtest_main.a /usr/local/lib/googletest/libgtest_main.a
 
 apt-get remove --yes cmake
